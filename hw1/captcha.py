ce53a2a6f502e651ff9d611737d48cf3d36eaa9a
Yifan <jameswu1212@gmail.com>
2018-03-19 23:20:13 +0800

    captcha offst

--- hw1/captcha.py
+++ hw1/captcha.py
@@ -17,12 +17,12 @@ def removeSault(img):
         string = ''
         for x in range(1, w - 2):
             count = 0
-            # for i in range(0, 8):
-            #     if pixel[x + dirx[i], y + diry[i]] > 254:
-            #         count += 1
+            for i in range(0, 8):
+                if pixel[x + dirx[i], y + diry[i]] > 254:
+                    count += 1
 
-            # if count >= 7:
-            #     pixel[x, y] = 255
+            if count >= 6:
+                pixel[x, y] = 255
 
             if pixel[x,y] > 140:
                 pixel[x, y] = 255;
@@ -31,25 +31,82 @@ def removeSault(img):
                 pixel[x, y] = 0;
                 string += '1'
 
-        print(string)
+    for x in range(0, w):
+        pixel[x, 0] = 255
+        pixel[x, h - 1] = 255
+    for y in range(0, h):
+        pixel[0, y] = 255
+        pixel[w - 1, y] = 255
+
     return img
 
 
 def move(img):
-    for x in range(1, 10):
-        pass
+    w, h = img.size
+    pixel = img.load()
+    max = 0
+
+    for i in range(0, 5)
+        offsetx = (i + 1) * 20
+
+        for x in range(0, 20 - 16 - 1):
+            for y in range(0, 50 - 16 - 1):
+                count = 0
+                for m in range(0, 16):
+                    for n in range(0, 16):
+                        if  pixel[m + x, n + y] == 0:
+                            count += 1
+
+                if count > max:
+                     max = count
+                     posx, posy = x, y
+        # print(posx, posy)
+
+        if posy + 8 <= 25:
+            offset = 25 - (posy + 8)
+            for x in range(posx, posx + 16):
+                for y in range(posy, posy + 16):
+                    pixel[x, y + offset] = pixel[x, y]
+                    pixel[x, y] = 255
+        else:
+            offset = (posy + 8) - 25
+            for x in range(posx, posx + 16):
+                for y in range(posy, posy + 16):
+                    pixel[x, y - offset] = pixel[x, y]
+                    pixel[x, y] = 255
+
+
+
+    # for y in range(posy, posy + 16):
+    #     string = ''
+    #     for x in range(posx, posx + 16):
+    #         pixel2[x % 16 + 2, y % 16 + 2] = pixel[x, y]
+
+    # im.show()
+
+    return img
+
+def decode(img):
+    code = tesserocr.image_to_text(img)
+    code = code.replace('\n', '')
+    print(code)
 
 
 # def Binarization():
 
 
-img = Image.open('captch' + str(68) + '.png').convert('L')
+img = Image.open('captcha' + str(num) + '.png').convert('L')
 
 img = removeSault(img)
+img.show()
+decode(img)
+
+img = move(img)
 
 img.show()
-code = tesserocr.image_to_text(img)
-code = code.replace('\n', '')
+decode(img)
 
-print(code)
+
+
+
 
